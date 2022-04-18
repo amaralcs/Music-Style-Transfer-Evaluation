8d80205d81ddad8ba7df1b7c8b6a3ed985af1f87
amaralcs <amaralcscgp@gmail.com>
2022-04-18 12:34:54 +0100

    Refactor code to remove intermediate steps that save the data

--- cyclegan/prepare_data.py
+++ cyclegan/prepare_data.py
@@ -1,28 +1,64 @@
+"""
+    
+
+"""
+
 import logging
 import os
-import shutil
-import json
 import errno
 import sys
-from functools import reduce
-
+from argparse import ArgumentParser
 import numpy as np
+
 from pypianoroll import Multitrack, Track, from_pretty_midi
 import pretty_midi
-import write_midi as wm
 
+import write_midi as wm
 from tf2_utils import save_midis
 
 logger = logging.getLogger("preprocessing_logger")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-ROOT_PATH = "test_preproc"
-GENRE_PATH = "Pop_Music_Midi"
-TEST_RATIO = 0.1
 
-converter_path = os.path.join(ROOT_PATH, "test/converter")
-cleaner_path = os.path.join(ROOT_PATH, "test/cleaner")
+def parse_args(argv):
+    """Parses input options for this module.
+
+    Parameters
+    ----------
+    argv : List
+        List of input parameters to this module
+
+    Returns
+    -------
+    Argparser
+    """
+    args = ArgumentParser()
+    args.add_argument(
+        "--root-path",
+        default="processed_midi",
+        type=str,
+        help="Path to where the midis are stored.",
+    )
+    args.add_argument(
+        "--outpath", default="phrases", type=str, help="Name of directory with outputs."
+    )
+    args.add_argument(
+        "--genre", default="pop", type=str, help="Genre of the midis being processes."
+    )
+    args.add_argument(
+        "--test-ratio",
+        default=0.3,
+        type=float,
+        help="Ratio of files to use for testing.",
+    )
+    args.add_argument(
+        "--remove-velocity",
+        default=True,
+        type=bool,
+        help="Whether to remove velocity information from the final outputs.",
+    )
+    return args.parse_args(argv)
 
 
 def get_midi_path(root):
@@ -52,10 +88,6 @@ def make_sure_path_exists(path):
     ----------
     path : str
         Name of folder to create.
-
-    Returns
-    -------
-    None
     """
     try:
         os.makedirs(path)
@@ -162,7 +194,7 @@ def get_merged_multitrack(multitrack):
     )
 
 
-def create_multitracks(filepath, multitrack_path):
+def create_multitracks(filepath):
     """Save a multi-track piano-roll converted from a MIDI file to target
     dataset directory and update MIDI information to `midi_dict`
 
@@ -190,20 +222,17 @@ def create_multitracks(filepath, multitrack_path):
         multitrack = from_pretty_midi(pm)
         merged = get_merged_multitrack(multitrack)
 
-        make_sure_path_exists(multitrack_path)
-        merged.save(os.path.join(multitrack_path, midi_name + ".npz"))
-
-        return [midi_name, midi_info]
+        return [midi_name, midi_info, merged]
 
     except TypeError:
         print(f"There was a type error when loading {midi_name}")
         return None
 
 
-def midi_filter(
+def filter_track(
     midi_info, beat_time=0, time_signature_changes=1, allowed_signatures=["4/4"]
 ):
-    """Return True for qualified midi files and False for unwanted ones.
+    """Returns True if the track is to be filtered, else returns False.
 
     Files are qualified based on `first_beat_time`, `num_time_signature_change` and
     `time_signature`.
@@ -214,51 +243,15 @@ def midi_filter(
     boolean
     """
     if midi_info["first_beat_time"] > beat_time:
-        return False
+        return True
     elif midi_info["num_time_signature_change"] > time_signature_changes:
-        return False
+        return True
     elif allowed_signatures:
         if midi_info["time_signature"] not in allowed_signatures:
-            return False
-    return True
+            return True
+    return False
 
-
-# Step 1
-def train_test_split(root_path, genre, test_ratio=TEST_RATIO):
-    """Splits the files in a given directory into training and test sets
-
-    Parameters
-    ----------
-    root_path : str
-    genre : str
-    test_ratio:
-        Ratio of files used for testing the model.
-    """
-    logger.info(f"Loading files from {os.path.join(root_path, genre)}")
-    filenames = [f for f in os.listdir(os.path.join(root_path, genre))]
-
-    test_fpath = os.path.join(root_path, "test", "origin_midi")
-    make_sure_path_exists(test_fpath)
-
-    idx = np.random.choice(
-        len(filenames), int(test_ratio * len(filenames)), replace=False
-    )
-    for i in idx:
-        shutil.move(
-            os.path.join(root_path, genre, filenames[i]),
-            os.path.join(test_fpath, filenames[i]),
-        )
-    logger.info(f"\t{len(idx)} files saved for test in {test_fpath}")
-
-
-# step 2
-def convert_and_clean_midis(
-    root_path,
-    midi_fpath="origin_midi",
-    filtered_fpath="filtered",
-    multitrack_path="multitrack",
-    **filter_kwargs,
-):
+def convert_and_clean_midis(midi_fpath, **filter_kwargs):
     """Loads the midis selected in the first step and converts them to pypianoroll.Multitrack and filters
     based on the given rules.
 
@@ -266,8 +259,6 @@ def convert_and_clean_midis(
 
     Parameters
     ----------
-    root_path : str
-        Path to midi file.
     midi_fpath : str
         Path to midi files.
     filtered_fpath : str
@@ -276,87 +267,31 @@ def convert_and_clean_midis(
         Output path for multitrack file.
     filter_kwargs : dict
         Options for filtering MIDI files.
+    **filter_kwargs,
+
     Returns
     -------
     None
     """
     # Create full path
-    midi_fpath = os.path.join(root_path, midi_fpath)
-    filtered_fpath = os.path.join(root_path, filtered_fpath)
-    multitrack_path = os.path.join(root_path, multitrack_path)
 
     # First step: load midis, create multitracks of each and save them
     midi_paths = get_midi_path(midi_fpath)
     logger.info(f"Found {len(midi_paths)} midi files")
 
     track_metadata = {}
-    midi_tracks = [
-        create_multitracks(midi_path, multitrack_path) for midi_path in midi_paths
-    ]
-    for (name, track) in midi_tracks:
-        if name is not None:
-            track_metadata[name] = track
+    midi_tracks = [create_multitracks(midi_path) for midi_path in midi_paths]
 
-    with open(os.path.join(root_path, "track_metadata.json"), "w") as outfile:
-        json.dump(track_metadata, outfile)
-    logger.info(
-        "[Done] {} files out of {} have been successfully converted".format(
-            len(track_metadata), len(midi_paths)
-        )
-    )
+    filtered_tracks = []
+    for (name, info, track) in midi_tracks:
+        if not filter_track(info, **filter_kwargs):
+            track.name = name
+            filtered_tracks.append(track)
 
-    # Second step: filter midis based on track information such as beat time, time signature changes..
-    count = 0
-    make_sure_path_exists(filtered_fpath)
-    clean_metadata = {}
-    for key in track_metadata:
-        if midi_filter(track_metadata[key], **filter_kwargs):
-            clean_metadata[key] = track_metadata[key]
-            count += 1
-            shutil.copyfile(
-                os.path.join(multitrack_path, key + ".npz"),
-                os.path.join(filtered_fpath, key + ".npz"),
-            )
+    logger.info(f"[Done] {len(filtered_tracks)} have been kept.")
+    logger.info("-" * 20)
 
-    with open(os.path.join(root_path, "filtered_track_metadata.json"), "w") as outfile:
-        json.dump(clean_metadata, outfile)
-    logger.info(
-        f"[Done] {count} files out of {len(track_metadata)} have been successfully cleaned"
-    )
-    logger.info("-"*20)
-
-
-# Step 3:
-# Choose the clean midi from the original dataset
-def copy_clean_data(midi_fpath="origin_midi", filtered_midi_fpath="filtered_midi"):
-    """ "Copies the filtered midis to the filtered folder.
-
-    Parameters
-    ----------
-    midi_fpath : str
-        Path to midi that have been selected for the test dataset.
-    filtered_midi_fpath : str
-        Name of folder to save the filtered midis to.
-
-    Returns
-    -------
-    None
-    """
-    make_sure_path_exists(os.path.join(ROOT_PATH, filtered_midi_fpath))
-
-    files_to_copy = os.listdir(os.path.join(ROOT_PATH, midi_fpath))
-    logger.info(f"Found {len(files_to_copy)} files to copy to {filtered_midi_fpath}")
-    for filename in files_to_copy:
-        shutil.copy(
-            os.path.join(
-                ROOT_PATH, midi_fpath, os.path.splitext(filename)[0] + ".midi"
-            ),
-            os.path.join(
-                ROOT_PATH, filtered_midi_fpath, os.path.splitext(filename)[0] + ".midi"
-            ),
-        )
-    logger.info("-"*20)
-
+    return filtered_tracks
 
 
 def shape_last_bar(piano_roll, last_bar_mode="remove"):
@@ -381,28 +316,23 @@ def shape_last_bar(piano_roll, last_bar_mode="remove"):
             piano_roll = np.delete(
                 piano_roll, np.s_[-int(piano_roll.shape[0] % 64) :], axis=0
             )
-    # Reshape to (n_bars, n_bars * n_time_steps, midi_range (128 notes))
+
+    # Reshape to (-1, n_bars * n_time_steps, midi_range (128 notes))
     return piano_roll.reshape(-1, 64, 128)
 
 
-# Step 4:
-# Merge and crop
-def trim_midi_files(
-    filtered_midi_fpath="filtered_midi",
-    clip_range=(24, 108),
-    drop_phrases=True,
-    midi_outpath="trimmed_midi",
-    npy_outpath="trimmed_npy",
-):
+def trim_midi_files(multitracks, clip_range=(24, 108), drop_phrases=True):
     """
 
     Parameters
     ----------
+    root_path : str
+        Path to where the midis are stored.
     filtered_midi_path : str, Optional
         Path the filtered midi files
     clip_range : tuple(int, int), Optional
         2-Tuple containing indices of midi range to clip. Use None if no clipping is desired.
-    drop_phrases : True, Optiona
+    drop_phrases : True, Optional
         If set to true, only keep tracks that have 4 bars or keep a single phrase of tracks that do not
         have number of tracks a multiple of 4.
 
@@ -411,28 +341,14 @@ def trim_midi_files(
     List
         A list with the trimmed files
     """
-    make_sure_path_exists(os.path.join(ROOT_PATH, midi_outpath))
-    make_sure_path_exists(os.path.join(ROOT_PATH, npy_outpath))
-
-    filtered_midi_fpath = os.path.join(ROOT_PATH, filtered_midi_fpath)
-    filtered_midis = os.listdir(filtered_midi_fpath)
-
     trimmed_midis = []
-    logger.info(f"Trimming {len(filtered_midis)} midi files...")
-    for midi_file in filtered_midis:
+    logger.info(f"Trimming {len(multitracks)} midi files...")
+
+    for multitrack in multitracks:
         # A dict to keep track of index of instruments in the tracks
         track_indices = {"Piano": [], "Drums": []}
-
-        track_name = os.path.splitext(midi_file)[0]
-        logger.info(f"\ttrimming {track_name}")
+        logger.debug(f"\ttrimming {multitrack.name}")
         try:
-            midi_obj = pretty_midi.PrettyMIDI(
-                os.path.join(filtered_midi_fpath, midi_file)
-            )
-            multitrack = from_pretty_midi(midi_obj)
-
-            multitrack.name = track_name
-
             # Find the indices of Piano and Drums
             for idx, track in enumerate(multitrack.tracks):
                 if track.is_drum:
@@ -440,82 +356,123 @@ def trim_midi_files(
                 else:
                     track_indices["Piano"].append(idx)
 
-                # Blend all piano tracks into a single track
-                blended_multitrack = Multitrack(
-                    tracks=[multitrack[idx] for idx in track_indices["Piano"]]
-                ).blend()
+            # Blend all piano tracks into a single track
+            blended_multitrack = Multitrack(
+                tracks=[multitrack[idx] for idx in track_indices["Piano"]]
+            ).blend()
 
-                shaped_multitrack = shape_last_bar(blended_multitrack)
-                # Clip the range of the instruments to the indices given
-                if clip_range:
-                    low, high = clip_range
-                    shaped_multitrack = shaped_multitrack[:, :, low:high]
+            shaped_multitrack = shape_last_bar(blended_multitrack)
 
-                # Either keep all bars, or a single phrase
-                if drop_phrases and (shaped_multitrack.shape[0] % 4 != 0):
-                    drop_bars_idx = int(shaped_multitrack.shape[0] % 4)
-                    shaped_multitrack = np.delete(
-                        shaped_multitrack, np.s_[-drop_bars_idx:], axis=0
-                    )
+            # Clip the range of the instruments to the indices given
+            if clip_range:
+                low, high = clip_range
+                shaped_multitrack = shaped_multitrack[:, :, low:high]
 
-                # Reshaped into (batchsize, 64, clipped_range, 1)
-                shaped_multitrack = shaped_multitrack.reshape(-1, 64, 84, 1)
-
-                # TODO: Understand exactly what's happening inside this
-                save_midis(
-                    shaped_multitrack,
-                    os.path.join(ROOT_PATH, midi_outpath, track_name + ".midi"),
+            # Either keep all bars, or a single phrase
+            if drop_phrases and (shaped_multitrack.shape[0] % 4 != 0):
+                drop_bars_idx = int(shaped_multitrack.shape[0] % 4)
+                shaped_multitrack = np.delete(
+                    shaped_multitrack, np.s_[-drop_bars_idx:], axis=0
                 )
-                np.save(
-                    os.path.join(ROOT_PATH, npy_outpath, track_name + ".npy"),
-                    shaped_multitrack,
-                )
-                trimmed_midis.append(shaped_multitrack)
+
+            # Reshaped into (batchsize, 64, clipped_range, 1)
+            shaped_multitrack = shaped_multitrack.reshape(-1, 64, 84, 1)
+
+            # # TODO: I'm not sure I need the processing that's happening inside `save_midis`
+            # save_midis(
+            #     shaped_multitrack,
+            #     os.path.join(root_path, midi_outpath, track_name + ".midi"),
+            # )
+            trimmed_midis.append(shaped_multitrack)
         except Exception as err:
-            logger.info(f"Error: {err}")
             raise err
-    logger.info("[Done]")            
+    logger.info("[Done]")
     return trimmed_midis
 
 
-def save_phrases(trimmed_midis, outpath="phrases", remove_velocity=True):
+def save_phrases(trimmed_midis, dataset, outpath, genre, remove_velocity=True):
     """Saves the processed midis as a number of npy arrays to the target path.
 
     Parameters
     ----------
     trimmed_midis : List[np.array]
         List of midi files to save.
-    outpath : str, Optional
+    dataset : str
+        Name of dataset to save (train or test)
+    outpath : str
         Location to save the phrases.
     remove_velocity : Boolean, Optional
-        Whether to remove velocity information from the outputs.    
+        Whether to remove velocity information from the outputs.
     """
-    outpath = os.path.join(ROOT_PATH, outpath)
+
+    outpath = os.path.join(outpath, dataset)
     make_sure_path_exists(outpath)
+
     concat_midis = np.concatenate(trimmed_midis, axis=0)
     logger.info(f"Concatenated files have shape: {concat_midis.shape}")
 
     # Convert to an array of booleans if we want to omit velocity
     if remove_velocity:
         concat_midis = concat_midis > 0
-    
+
     logger.info(f"Saving phrases to {outpath}")
     for idx, phrase in enumerate(concat_midis):
-        np.save(
-            os.path.join(outpath, f"phrase_{idx+1}"),
-            phrase
-        )
+        np.save(os.path.join(outpath, f"{genre}_{idx+1}"), phrase)
+
+def train_test_split(tracks, test_ratio):
+    """Splits the files in a given directory into training and test sets
+
+    Parameters
+    ----------
+    root_path : str
+        Path to where the midis are stored.
+    genre : str
+        Genre of songs being processed. Should match a directory name in the `root_path`.
+    test_ratio:
+        Ratio of files used for testing the model.
+    dataset : str
+        Type of dataset being created. One of ["train", "test"]
+
+    Returns
+    -------
+    train_set, test_set
+    """
+    test_indices = np.random.choice(
+        len(tracks), int(test_ratio * len(tracks)), replace=False
+    )
+    train_indices = list(set(range(len(tracks))).difference(set(test_indices)))
+    logger.info(
+        f"Using {len(train_indices)} files for training and {len(test_indices)} for testing."
+    )
+
+    # Convert to np array for easy slicing.
+    # Adding dtype="object" prevents a warning
+    tracks = np.array(tracks, dtype="object")
+
+    train_set = tracks[train_indices]
+    test_set = tracks[test_indices]
+
+    return train_set, test_set
+
+
 
 def main(argv):
-    train_test_split(ROOT_PATH, GENRE_PATH)
-    convert_and_clean_midis(ROOT_PATH + "/test")
-    copy_clean_data("test/origin_midi", "test/filtered_midi")
-    trimmed_midis = trim_midi_files(
-        filtered_midi_fpath="test/filtered_midi",
-        midi_outpath="test/trimmed_midi",
-        npy_outpath="test/trimmed_npy"
-    )
-    save_phrases(trimmed_midis)
+    args = parse_args(argv)
+    root_path = args.root_path
+    outpath = args.outpath
+    genre = args.genre
+    test_ratio = args.test_ratio
+    remove_velocity = args.remove_velocity
+
+    midi_fpaths = os.path.join(root_path, genre)
+    tracks = convert_and_clean_midis(midi_fpaths)
+    trimmed_tracks = trim_midi_files(tracks)
+    train_set, test_set = train_test_split(trimmed_tracks, test_ratio)
+
+    outpath = os.path.join(root_path, outpath)
+    save_phrases(train_set, "train", outpath, genre, remove_velocity)
+    save_phrases(test_set, "test", outpath, genre, remove_velocity)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
