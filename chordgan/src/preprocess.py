40d1cd3a2b377b8fe62af9d3e7cd64321799e6c9
amaralcs <amaralcscgp@gmail.com>
2022-05-10 23:13:17 +0100

    Refactor `preprocess` logic
    This commit introduces a departure from the original preprocessing method.
    Here we follow steps to transform the data similarly to what is done in CycleGAN.
    
    The main reason for this is because I believe the original processing method was
    erroneous and accidentally introducing noise

--- chordgan/src/preprocess.py
+++ chordgan/src/preprocess.py
@@ -1,9 +1,6 @@
-import sys
 import numpy as np
 import pretty_midi
-import reverse_pianoroll
-import convert
-import argparse
+# import convert
 from glob import glob
 import os
 import logging
@@ -14,8 +11,12 @@ logger = logging.getLogger("preprocessing_logger")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-def get_chroma(song, chroma_dims=12, n_notes=78):
+
+def get_chroma(song, chroma_dims=12, n_notes=84):
     """Creates the chromagram of a given song.
+
+    Implementation taken from:
+    https://www.audiolabs-erlangen.de/resources/MIR/FMP/C3/C3S1_SpecLogFreq-Chromagram.html
 
     Parameters
     ----------
@@ -30,42 +31,86 @@ def get_chroma(song, chroma_dims=12, n_notes=78):
     -------
     chroma
     """
-    chroma = np.zeros(shape=(song.shape[0], chroma_dims))  # 12 chroma values
-    for i in np.arange(song.shape[0]):
-        for j in np.arange(n_notes):
-            if song[i][j] > 0:
-                chroma[i][np.mod(j, chroma_dims)] += 1
+    chroma = np.zeros(shape=(song.shape[0], chroma_dims))
+    p = np.arange(n_notes)
+
+    # for each pitch class
+    for c in range(12):
+        # create a mask for note indices that belong to this class
+        mask = (p % 12) == c
+        # calculate the chroma as the sum of all pitches in this class
+        chroma[:, c] = song[:, mask].sum(axis=1)
     return chroma
 
 
-def reshape_step(input_, input_timesteps, n_timesteps=4):
-    """Applies ChordGAN reshaping to a given input
+def reshape_step(midi, n_sequences, total_timesteps):
+    """Trims the song 
 
     Parameters
     ----------
-    input_ : pypianoroll
+    midi : np.array
         Song or chroma pianoroll.
-    input_timesteps : int
-        Number of timesteps to reshape the song to. Varies depending with song length.
-    n_timestes: int
-        The number of bars to limit each song to.
+    n_sequences : int
+        Number of sequences of size `n_bars` * `fs` that can be fit exactly in the given
+        input midi.
+    total_timesteps: int
+        Total number of timesteps in the number of given bars times # of notes per bar.
 
     Returns
     -------
     input_
     """
-    input_ = input_[: input_timesteps * n_timesteps]  # discard any extra timesteps
-    input_ = input_.reshape([input_timesteps, input_.shape[1] * n_timesteps])
-    return input_
+    return midi[: n_sequences * total_timesteps]  # discard any extra timesteps
 
 
-def get_songs(path, n_timesteps=4):
+def clip_midi(midi, low=24, high=108):
+    """Clips the given midi to a specified range of notes.
+
+    Clipping is done along the first axis and the default range is C1 (24) - C8 (108).
+
+    Parameters
+    ----------
+    midi : np.array
+        The midi array to clip.
+    low : int
+    high : int
+
+    Returns
+    -------
+    np.array
+    """
+    return midi[low:high, :]
+
+
+def normalize_velocities(midi):
+    """Sets all non-zero velocities of the midi file to 1.
+
+    Parameters
+    ----------
+    midi : np.array
+        The midi array to normalize.
+
+    Returns
+    -------
+    np.array
+    """
+    idx_x, idx_y = midi.nonzero()
+    midi[idx_x, idx_y] = 1
+    return midi
+
+
+def get_songs(path, n_bars=4, fs=16):
     """Loads midi files from the given path and converts them to piano roll format.
 
     Parameters
     ----------
     path : str
         Path to directory containing MIDI files
+    n_bars : int
+        The number of bars that each song sequence will have.
+    fs : int
+        The sampling rate when generating the piano roll. It is also the number of 
+        notes that each bar will contain.
 
     Returns
     -------
@@ -77,17 +122,28 @@ def get_songs(path, n_timesteps=4):
     """
     files = glob("{}/*.mid*".format(path))
     songs, fnames, chromas = [], [], []
+
+    total_timesteps = n_bars * fs
     for f in files:
         try:
             data = pretty_midi.PrettyMIDI(f)
-            song = data.get_piano_roll(fs=16)
-            song = convert.forward(song)
+            song = data.get_piano_roll(fs=fs)
+            song = clip_midi(song)
+            song = normalize_velocities(song)
+            song = song.T  # Transpose the song to (n_notes, note_range)
+
             chroma = get_chroma(song)
 
-            # Reshaping steps
-            song_timesteps = song.shape[0] // n_timesteps
-            song = reshape_step(song, song_timesteps)
-            chroma = reshape_step(chroma, song_timesteps)
+            # calculate the number of sequences of size n_bars * fs 
+            # we can use to split this song into
+            n_sequences = song.shape[0] // total_timesteps
+
+            # Then use it to reshape the song and chromas
+            # Note: At the moment this simply reshapes the song and treats it as a whole
+            # Another approach to be tried is to split the song into `n_sequences` and add 
+            # each sequence individually to the training set. TODO
+            song = reshape_step(song, n_sequences, total_timesteps)
+            chroma = reshape_step(chroma, n_sequences, total_timesteps)
 
             songs.append(song)
             chromas.append(chroma)
@@ -114,7 +170,7 @@ def create_dataset(tensor_list):
     return reduce(lambda ds1, ds2: ds1.concatenate(ds2), datasets)
 
 
-def join_datasets(song_ds, chroma_ds, shuffle_buffer=10000):
+def join_datasets(song_ds, chroma_ds, shuffle, shuffle_buffer=10000):
     """Joins two given datasets to create inputs of the form ((s1, c1), (s2, c2), ...)
 
     Parameters
@@ -123,6 +179,10 @@ def join_datasets(song_ds, chroma_ds, shuffle_buffer=10000):
         Dataset with songs.
     chroma_ds : tf.data.Dataset
         Dataset with song chromas.
+    shuffle : bool
+        Whether to shuffle the resulting dataset. Setting it to False is useful when
+        processing a dataset for genre transfer as the songs will retain their order
+        and can be identified with the `names` output.
     shuffle_buffer : int, Optional
         The size of the shuffle buffer
 
@@ -139,28 +199,31 @@ def join_datasets(song_ds, chroma_ds, shuffle_buffer=10000):
     it fits the model inputs
 
     """
-    return (
-        tf.data.Dataset.zip((song_ds, chroma_ds))
-        .shuffle(shuffle_buffer)
-        .map(
-            lambda song, chroma: (
-                tf.expand_dims(song, axis=0),
-                tf.expand_dims(chroma, axis=0),
-            )
+    ds = tf.data.Dataset.zip((song_ds, chroma_ds)).map(
+        lambda song, chroma: (
+            tf.expand_dims(song, axis=0),
+            tf.expand_dims(chroma, axis=0),
         )
-        .prefetch(1)
     )
+    if shuffle:
+        ds = ds.shuffle(shuffle_buffer)
+
+    return ds.prefetch(1)
 
 
-def load_data(fpath, genre):
+def load_data(fpath, genre, shuffle=True):
     """Loads and preprocess the data as required for ChordGAN
-    
+
     Parameters
     ----------
     fpath : str
         Path to where the data is stored.
     genre : str
         Genre of dataset to be loaded.
+    shuffle : bool
+        Whether to shuffle the resulting dataset. Setting it to False is useful when
+        processing a dataset for genre transfer as the songs will retain their order
+        and can be identified with the `names` output.
 
     Returns
     -------
@@ -174,4 +237,4 @@ def load_data(fpath, genre):
     chromas_ds = create_dataset(chromas)
 
     logger.info("Complete.")
-    return join_datasets(song_ds, chromas_ds)
+    return join_datasets(song_ds, chromas_ds, shuffle), (songs, names)
